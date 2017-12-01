ec8f7ae96fcf4dc453c46eca08db52acb7a94c7c
eggyy1224 <eggyy1224@gmail.com>
2017-12-01 15:28:36 +0800

    add restaurant controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'restaurants/index'
+
+  root "restaurants#index" #把首頁設為restaurant控制器的index動作
 end
