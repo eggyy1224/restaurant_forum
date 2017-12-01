adb3a4e19f46ab4b221b8f291737942cf359e094
eggyy1224 <eggyy1224@gmail.com>
2017-12-01 15:51:17 +0800

    add admin controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'restaurants/index'
+  end
+
   get 'restaurants/index'
 
   root "restaurants#index" #把首頁設為restaurant控制器的index動作
+
+  namespace :admin do
+    root "restaurants#index"
+  end
 end
