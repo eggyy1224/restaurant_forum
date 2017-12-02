203613e10528f7c27fbc25cd65478c97d08b580f
eggyy1224 <eggyy1224@gmail.com>
2017-12-02 13:29:17 +0800

    add user admin log_in integration tests

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  def setup
+    @user = users(:one)
+    @user2 = users(:two)
+  end
+  test 'login as normal user' do
+
+    sign_in @user2
+    get restaurants_index_path
+    assert_template 'restaurants/index'
+
+    get admin_restaurants_index_path#非管理員進不去後台
+    assert_redirected_to root_path
+    
+    
+  end
+
+  test 'login as admin user' do
+
+    sign_in @user
+    get restaurants_index_path
+    assert_template root_path
+
+    get admin_restaurants_index_path#非管理員進不去後台
+    #管理員身份可以進去後臺
+    assert_template 'admin/restaurants/index'
+    
+    
+  end
+end
--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
   test "Restaurant model" do
-    assert Restaurant.new(name: "123")
+    
   end
 end
