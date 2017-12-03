a1a8a40787489b263811073c7d1569ba99bc6284
eggyy1224 <eggyy1224@gmail.com>
2017-12-03 14:49:23 +0800

    add restaurant update action test

--- test/controllers/admin/restaurants_controller_test.rb
+++ test/controllers/admin/restaurants_controller_test.rb
@@ -1,6 +1,25 @@
 require 'test_helper'
 
 class Admin::RestaurantsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  def setup
+    @user = users(:one)
+    @restaurant = restaurants(:fattys)
+  end
   
+
+  test "update params should have name " do
+    sign_in @user
+    patch admin_restaurant_path(@restaurant), params: { restaurant: { name: '' } }
+    assert_template 'admin/restaurants/edit'
+    assert_not flash.empty?
+
+    patch admin_restaurant_path(@restaurant), params: { restaurant: { name: '123' } }
+    assert_redirected_to admin_restaurant_path(@restaurant)
+    assert_not flash.empty?
+
+  end
+
+
 
 end
