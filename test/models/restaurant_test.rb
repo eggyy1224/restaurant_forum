f82b7d7687b831b93323bbb64c9d1eb629be6e3b
eggyy1224 <eggyy1224@gmail.com>
2017-12-03 12:31:13 +0800

    add restaurant model name presence test

--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
-  test "Restaurant model" do
+  test "Restaurant model validates name" do
     
+    assert_no_difference 'Restaurant.count' do
+      restaurant = Restaurant.new(name: "")
+      restaurant.save
+    end
+
+    assert_difference 'Restaurant.count', 1 do
+      restaurant = Restaurant.new(name: "123")
+      restaurant.save
+    end
   end
 end
