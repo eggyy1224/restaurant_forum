d6659d8e9775eafcc7150892c0e427ca09b8bfd0
eggyy1224 <eggyy1224@gmail.com>
2017-12-04 11:03:39 +0800

    add index pagination test

---
+++ test/integration/index_page_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class IndexPageTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    @user = users(:one)
+  end
+
+  test 'index page should have pagination' do
+    sign_in @user
+
+    get admin_restaurants_path
+    
+    assert_select 'nav.pagination', count: 2
+  end
+end
