f5777e92eb11873f23f90f5747197ccefb3399f8
eggyy1224 <eggyy1224@gmail.com>
2017-12-12 11:59:51 +0800

    setup restaurant resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   root "restaurants#index" #把首頁設為restaurant控制器的index動作
 
+  resources :restaurants, only: [:index, :show]
+
   namespace :admin do
     resources :restaurants
     resources :categories
