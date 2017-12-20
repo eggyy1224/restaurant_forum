4d4d8c0ac4d51799c1261035e26a4bdfddded69c
eggyy1224 <eggyy1224@gmail.com>
2017-12-20 12:38:21 +0800

    add user edit and update

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+
+    @user = User.find(params[:id]) 
+    unless current_user == @user
+      redirect_to current_user
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params) 
+      flash[:notice] = "成功更新個人資料！"
+      redirect_to @user
+    else
+      flash[:danger] = "資料更新失敗！"
+      render :edit
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :intro, :avatar)
+    end
+end
