b000b15f240c50627c8351652eef103acfd0f8b5
eggyy1224 <eggyy1224@gmail.com>
2017-12-26 22:21:43 +0800

    add favorite routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,15 @@ Rails.application.routes.draw do
 
   resources :restaurants, only: [:index, :show] do
     resources :comments, only: [:create, :destroy]
+    #瀏覽所有餐廳得最新動態
     collection do
       get :feeds
     end
-
+    #查看個別餐廳的佈告欄
     member do
       get :dashboard
+      post :favorite
+      post :unfavorite
     end
   end
 
