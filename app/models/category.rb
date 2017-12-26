d23bf8beabc7a5645a02976f1916da84223580cb
eggyy1224 <eggyy1224@gmail.com>
2017-12-26 21:28:42 +0800

    add dashboard of restaurant

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-  has_many :restaurants, dependent: :destroy
-  validates :name, dependent: :restrict_with_error
+  has_many :restaurants, dependent: :restrict_with_error
+  validates :name, presence: true
 end
