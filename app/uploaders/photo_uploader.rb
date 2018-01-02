63dcbb2f41d6fb5524d56a4110a490dc88d7ee8b
eggyy1224 <eggyy1224@gmail.com>
2018-01-02 12:55:19 +0800

    modify file upload path in production

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -6,11 +6,9 @@ class PhotoUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  
+  storage :file
+  
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
